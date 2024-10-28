06f71323c91cbba46ed5cebae94dc90e7bd18885
Mitch Nyayieka <joynyayieka@gmail.com>
2024-10-28 21:19:22 +0300

    Updated exercises

---
+++ controlstatements.py
@@ -0,0 +1,18 @@
+temperature = 45
+
+if temperature > 25:
+    print("It is too hot")
+else:
+    print("It is too cold")
+
+#A python program that checks three numbers and returns the smallest one
+num1 = 78
+num2 = 15
+num3 = 90
+
+if num1 < num2 and num1 < num3:
+    print(num1, "is the smallest number")
+elif num2 < num1 and num2 < num3:
+    print(num2, "is the smallest number")
+else:
+    print(num3, "is the smallest number")
